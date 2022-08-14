52c83521c890afdb72bac5374849dcb443ae0d2a
nishyjana <nishyjana6435@gmail.com>
2022-08-14 23:54:25 +0530

    refactor

--- src/components/atom/Experince.tsx
+++ src/components/atom/Experince.tsx
@@ -2,12 +2,12 @@ export default function Experience() {
   return (
     <div className="w-full h-full p-10">
       <div className="border-2 border-orange border-opacity-10 p-10  text-orange text-opacity-80 rounded-3xl flex-col">
-        <div className="my-1 text-lg"> 🟡 Rootcode Labs </div>
-        <div className="my-1 text-sm"> Full time 1yr 11 months </div>
+        <div className="my-1 text-lg"> 🟡 Rootcode Labs ( 2021 - present)</div>
+        <div className="my-1 text-sm"> Full time 1yr 8 months </div>
         <div className="list-inside">
           <div className="pl-5">
             <div className="my-1 text-lg"> ○ Software Engineer </div>
-            <div className="my-1 text-sm"> Full time 1yr 5 months </div>
+            <div className="my-1 text-sm"> Full time 1yr 3 months </div>
             <div className="my-1">
               Skills: Amazon S3 · Front-End Development · REST APIs · Redux.js ·
               Apollo GraphQL · Git · GraphQL · Software Development · TypeScript
@@ -23,6 +23,17 @@ export default function Experience() {
             </div>
           </div>
         </div>
+        <div className="my-1 text-lg"> 🟡 FreeLance Web developer (2019 - 2020) </div>
+        <div className="my-1 text-sm"> Full time 1yr 5 months </div>
+        <div className="pl-5">
+            <div className="my-1 text-lg"> ○ Free Lance web developer </div>
+            <div className="my-1 text-sm"> Full time 1yr 5 months </div>
+            <div className="my-1">
+              Skills: Amazon S3 · Front-End Development · REST APIs · Redux.js ·
+              Apollo GraphQL · Git · GraphQL · Software Development · TypeScript
+              · Web Development · MERN Stack · React.js · MySQL
+            </div>
+          </div>
       </div>
     </div>
   );
