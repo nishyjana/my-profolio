2157117df9f5ca00bb932acdec5c61bebf81945c
nishyjana <nishyjana6435@gmail.com>
2022-08-14 23:50:00 +0530

    profile update changes

--- src/components/constants/Resources.ts
+++ src/components/constants/Resources.ts
@@ -1,4 +1,4 @@
-export const MyImageURl = 'https://scontent.fcmb11-1.fna.fbcdn.net/v/t39.30808-6/291333228_2943346645962949_4221671178690896799_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=kLd4i0viajQAX8-ON-Y&_nc_ht=scontent.fcmb11-1.fna&oh=00_AT_5gF0ZkC5RfsQpyn9EM3M5sGxb3vetPVI5TWEVYPNHzQ&oe=62FCB98A'
+export const MyImageURl = 'https://pbs.twimg.com/profile_images/1432392047483179012/gwifNDjk_400x400.jpg'
 
 export const FACEBOOK_ICON_URL = 'M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z'
 
