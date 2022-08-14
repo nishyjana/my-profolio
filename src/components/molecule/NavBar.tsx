c13f146687606b21698a9aca3cac52ec7f7dc6ef
nishyjana <nishyjana6435@gmail.com>
2022-08-15 00:44:31 +0530

    changes

--- src/components/molecule/NavBar.tsx
+++ src/components/molecule/NavBar.tsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { ABOUT_ME, CONSULTANCE, CONTACT_ME } from "../constants/NavIndex";
-import { ABOUT_ME_PATH, CONTACT_ME_PATH, HOME_PAGE_PATH } from "../constants/PathIndex";
+import {
+  ABOUT_ME_PATH,
+  CONTACT_ME_PATH,
+  HOME_PAGE_PATH,
+} from "../constants/PathIndex";
 
 interface Props {
   navProps: any;
@@ -10,31 +14,40 @@ export default function NavBar({ navProps }: Props) {
   const navigate = useNavigate();
   return (
     <div className="px-2 py-8 w-full text-white font-serif bg-black flex justify-between">
-      <div className="flex justify-start px-5 cursor-pointer hover:text-orange hover:text-xl" onClick={()=> navigate(HOME_PAGE_PATH)}>MY PORTFOLIO</div>
+      <div
+        className="flex justify-start px-5 cursor-pointer hover:text-orange hover:text-xl"
+        onClick={() => navigate(HOME_PAGE_PATH)}
+      >
+        MY PORTFOLIO
+      </div>
       <div className="flex px-5">
-      {navProps?.length
-        ? navProps?.map((prop: any, index:any) => {
-            return (
-              <div
-                className={`flex justify-end hover:text-xl px-4 hover:text-orange cursor-pointer ${
-                  prop === CONSULTANCE
-                    ? "py-2 -mt-2 border-2 border-blue bg-orange  text-blue rounded-3xl hover:text-base hover:text-blue"
-                    : null
-                }`}
-                key={index}
-                onClick={() => {
-                  if (prop === ABOUT_ME) {
-                    navigate(ABOUT_ME_PATH);
-                  } else if (prop === CONTACT_ME) {
-                    navigate(CONTACT_ME_PATH);
-                  }
-                }}
-              >
-                {prop}
-              </div>
-            );
-          })
-        : null}
+        {navProps?.length
+          ? navProps?.map((prop: any, index: any) => {
+              return (
+                <div
+                  className={`flex justify-end hover:text-xl px-4 hover:text-orange cursor-pointer ${
+                    prop === CONSULTANCE
+                      ? "py-2 -mt-2 border-2 border-blue bg-orange  text-blue rounded-3xl hover:text-base hover:text-blue"
+                      : null
+                  }`}
+                  key={index}
+                  onClick={() => {
+                    if (prop === ABOUT_ME) {
+                      navigate(ABOUT_ME_PATH);
+                    } else if (prop === CONTACT_ME) {
+                      navigate(CONTACT_ME_PATH);
+                    } else if (prop === CONSULTANCE) {
+                      alert(
+                        "Very sorry for the trouble, Still under development. Please contact me through nishyjana6435@gmail.com"
+                      );
+                    }
+                  }}
+                >
+                  {prop}
+                </div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
--- src/components/pages/ContactMe.tsx
+++ src/components/pages/ContactMe.tsx
@@ -1,3 +1,12 @@
 export default function ContactMe() {
-  return <div className="text-white">contact me</div>;
+  return (
+    <div className="h-screen w-full bg-black border-2 border-orange rounded-3xl">
+      <div className="text-orange text-center p-10">Under development</div>
+      <img
+        alt="progress"
+        className="m-auto my-10 opacity-60 w-1/3 h-1/3 rounded-full "
+        src="https://underconstructionpage.com/wp-content/uploads/2017/07/underconstruction-twitter.png"
+      />
+    </div>
+  );
 }
