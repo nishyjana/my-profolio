016af9a84d2432389bffe3c361e77f0229ce7170
nishyjana <nishyjana6435@gmail.com>
2022-08-15 13:57:14 +0530

    changes

--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { ABOUT_ME_PATH, HOME_PAGE_PATH } from "../constants/PathIndex";
+import { GITHUB_LINK } from "../constants/Resources";
 
 export default function Welcome() {
   const navigate = useNavigate();
@@ -27,6 +29,7 @@ export default function Welcome() {
         <p className="text-orange text-center text-sm p-10 animate-pulse">
           Thank you for stopping by! This is my personal website.
         </p>
+
         <div className="flex w-full">
           <button
             className="rounded-3xl border-2 border-black bg-orange text-black px-5 py-3 mx-auto"
@@ -34,6 +37,17 @@ export default function Welcome() {
           >
             Get Started
           </button>
+        </div>
+        <div className="flex w-full mt-20">
+          <p className="text-orange text-center text-lg  m-auto p-10 animate-pulse flex">
+            You can find the source code base of this webiste
+            <div
+              className="text-white text-xl mx-2  cursor-pointer"
+              onClick={() => (window.location.href = `${GITHUB_LINK}`)}
+            >
+              here!!!
+            </div>
+          </p>
         </div>
       </div>
     </div>
